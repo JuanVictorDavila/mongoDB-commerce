e6df0c2dc979e14d6836a6c20491c1df49446575
juan victor <juanvictor70@gmail.com>
2022-07-11 16:43:46 -0400

    feature:desafio4

---
+++ challenges/desafio4.js
@@ -0,0 +1,3 @@
+// Retorne os lanches que tiveram vendas maiores que 50 e menores que 100,
+// mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente
+db.produtos.find({ $and: [{ vendidos: { $gt: 50, $lt: 100 } }] }, { nome: true, vendidos: true, _id: false }).sort({ vendidos: 1 });
