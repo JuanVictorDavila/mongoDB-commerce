2f68a7f71541dbe6a0c548d946a726cafde5e22d
juan victor <juanvictor70@gmail.com>
2022-07-11 16:39:54 -0400

    style: Adicionando descrição dos desafios nos arquivos desafio1, desafio2, desafio3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,1 +1,3 @@
+// Ordene a coleção produtos pela quantidade de lanches vendidos em ordem crescente,
+// mostrando apenas o nome e a quantidade de lanches vendidos
 db.produtos.find({}, { nome: true, vendidos: true, _id: false }).sort({ vendidos: 1 });
--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,2 @@
+// Retorne o lanche mais vendido, mostrando apenas o nome e a quantidade do lanche mais vendido
 db.produtos.find({}, { nome: true, vendidos: true, _id: false }).sort({ vendidos: -1 }).limit(1);
