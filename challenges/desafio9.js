2ab3b76a821d653aea1086529a9439c196fd0e6a
juan victor <juanvictor70@gmail.com>
2022-07-11 18:08:10 -0400

    fix: corrigindo lint desafio9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,6 @@
 // Retorne o nome de todos os lanches que possuam calorias abaixo de 500
 db.produtos.find({
-  valoresNutricionais:{ $elemMatch:
-    { tipo: "calorias", quantidade: { $lt: 500 } }
+  valoresNutricionais: { $elemMatch:
+    { tipo: "calorias", quantidade: { $lt: 500 } },
   },
 }, { _id: false, nome: true });
