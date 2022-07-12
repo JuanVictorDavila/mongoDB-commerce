65aa0a65c949db40c22935ec8e689ce3a5a326c5
juan victor <juanvictor70@gmail.com>
2022-07-11 20:06:47 -0400

    feature:desafio17

---
+++ challenges/desafio17.js
@@ -0,0 +1,9 @@
+// Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos
+db.resumoProdutos.insertOne(
+  { franquia: "McDonalds", totalProdutos: db.produtos.find().count() },
+);
+
+db.resumoProdutos.find(
+    { franquia: "McDonalds" },
+    { _id: 0, franquia: 1, totalProdutos: 1 },
+);
