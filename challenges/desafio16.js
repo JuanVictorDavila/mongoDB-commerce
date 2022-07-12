2405769ab6782cf3e2d3e858fb4c94bcddc45cb7
juan victor <juanvictor70@gmail.com>
2022-07-11 20:01:59 -0400

    fix: corrigindo lint desafio16

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,10 +1,4 @@
 // Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac
-db.produtos.updateOne(
-  { nome: "Big Mac" },
-  { $set: { ultimaModificacao: new Date() } }
-);
+db.produtos.updateOne({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } });
 
-db.produtos.find(
-  { ultimaModificacao: { $exists: true } },
-  { _id: 0, nome: 1 }
-);
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
